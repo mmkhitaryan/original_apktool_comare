e4b10a0fd7ba3b24ff5075b6f1c00057e5df6efa
Marat Mkhitaryan <marat@mkhitaryan.pw>
2023-05-15 08:55:05 +0400

    still needs to be debugged

--- apktool_compare.sh
+++ apktool_compare.sh
@@ -4,9 +4,11 @@ md5deep -rl apk_extract | sort -k2 > original_apk_hashes.md5
 rm -rf apk_extract
 
 #
-java -jar apktool_2.7.0.jar d $1 $2 -o apk_extract
-java -jar apktool_2.7.0.jar b $1 -o apk_extract
+java -jar apktool_2.7.0.jar d $1 $2 -o apktool_extract
+java -jar apktool_2.7.0.jar b apktool_extract -o apk_extract
 rm -rf apktool_extracted
 md5deep -rl apk_extract | sort -k2 > after_apktool_apk_hashes.md5
+
+rm -rf apk_extract
 
 diff original_apk_hashes.md5 after_apktool_apk_hashes.md5
